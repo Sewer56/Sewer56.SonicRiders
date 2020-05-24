6bc6d773d1f876a8d5894986d5bb0a9b9b580d65
Sewer <seweratwork@gmail.com>
2020-05-24 02:56:35 +0100

    Add conversion between speedometer and real float value.

--- Sewer56.SonicRiders/Mathematics/Formula.cs
+++ Sewer56.SonicRiders/Mathematics/Formula.cs
@@ -12,6 +12,8 @@ namespace Sewer56.SonicRiders.Mathematics
     /// </summary>
     public static unsafe class Formula
     {
+        private const float speedToSpeedometerRatio = 216.0f;
+
         /// <summary>
         /// Calculates the speed of an individual extreme gear for a specific formation.
         /// </summary>
@@ -20,10 +22,22 @@ namespace Sewer56.SonicRiders.Mathematics
         public static float GetGearSpeed(ExtremeGear* extremeGear, FormationTypes formationType)
         {
             float characterTypeSpeedMultiplier = Players.CharacterTypeStats[(int)formationType].MaxSpeedMultiplier;
-            float speed = 216 * (characterTypeSpeedMultiplier + extremeGear->SpeedHandlingMultiplier);
+            float speed = speedToSpeedometerRatio * (characterTypeSpeedMultiplier + extremeGear->SpeedHandlingMultiplier);
 
             // Apply Speed Multiplier
             return speed * (extremeGear->SpeedMultiplier + 1);
         }
+
+        /// <summary>
+        /// Converts a speed in float to its value in-game on the speedometer.
+        /// </summary>
+        /// <param name="speed">The speed to convert to speedometer.</param>
+        public static float SpeedToSpeedometer(float speed) => speed * speedToSpeedometerRatio;
+
+        /// <summary>
+        /// Converts a speed in speedometer to its true float value.
+        /// </summary>
+        /// <param name="speed">The speed to convert to float.</param>
+        public static float SpeedometerToFloat(float speed) => speed / speedToSpeedometerRatio;
     }
 }
