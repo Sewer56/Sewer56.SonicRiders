30eb38a36367cbbca5c657de72e6002ce46ebe60
Sewer <seweratwork@gmail.com>
2020-06-05 18:00:30 +0100

    Update CourseSelectTaskState

---
+++ Sewer56.SonicRiders/Structures/Menus/Enums/CourseSelectTaskState.cs
@@ -0,0 +1,18 @@
+﻿namespace Sewer56.SonicRiders.Structures.Menus.Enums
+{
+    /// <summary>
+    /// Individual states for the <see cref="CourseSelect"/> tasks..
+    /// </summary>
+    public enum CourseSelectTaskState : byte
+    {
+        MainMenu = 1,
+        Normal = 3,
+        Submenu = 5,
+        Exit = 6,
+        BattleSelect = 7,
+        GrandPrix = 8,
+        OpenRaceRules = 9,
+        RaceRules = 10,
+        ShopMenu = 15
+    }
+}
