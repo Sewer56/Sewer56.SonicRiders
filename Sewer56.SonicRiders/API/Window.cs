96b2c86c797f935d46cc75608dca546d113a094e
Sewer56 <seweratwork@gmail.com>
2021-01-14 01:01:48 +0000

    Improve method of obtaining method handle to handle multi-instance.

--- Sewer56.SonicRiders/API/Window.cs
+++ Sewer56.SonicRiders/API/Window.cs
@@ -1,16 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
+using ProcessThread = System.Diagnostics.ProcessThread;
 
 namespace Sewer56.SonicRiders.API
 {
     public static class Window
     {
-        /// <summary>
-        /// The class associated with the Sonic Heroes window.
-        /// </summary>
         public const string WindowClass = "SonicRidersClass";
-
         private static IntPtr _cachedWindowHandle;
         private static Process _currentProcess = Process.GetCurrentProcess();
 
@@ -24,7 +24,7 @@ namespace Sewer56.SonicRiders.API
                 if (IsWindow(_cachedWindowHandle))
                     return _cachedWindowHandle;
 
-                _cachedWindowHandle = FindWindow(WindowClass, null);
+                _cachedWindowHandle = EnumerateThisProcessWindowHandles().FirstOrDefault();
                 return _cachedWindowHandle;
             }
         }
@@ -43,7 +43,30 @@ namespace Sewer56.SonicRiders.API
             return activeProcessId == _currentProcess.Id;
         }
 
+        private static IEnumerable<IntPtr> EnumerateThisProcessWindowHandles()
+        {
+            IntPtr handle = IntPtr.Zero;
+            foreach (ProcessThread thread in _currentProcess.Threads)
+            {
+                handle = IntPtr.Zero;
+                EnumThreadWindows(thread.Id, (hWnd, lParam) =>
+                {
+                    var stringBuilder = new StringBuilder(256);
+                    GetClassName(hWnd, stringBuilder, 256);
+                    if (stringBuilder.ToString().Equals(WindowClass, StringComparison.OrdinalIgnoreCase))
+                        handle = hWnd;
+
+                    return true;
+                }, IntPtr.Zero);
+                
+                if (handle != IntPtr.Zero)
+                    yield return handle;
+            }
+        }
+
         /* Windows API Imports */
+        delegate bool EnumThreadDelegate(IntPtr hWnd, IntPtr lParam);
+
         #region MyRegion
         [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
         private static extern IntPtr GetForegroundWindow();
@@ -52,11 +75,14 @@ namespace Sewer56.SonicRiders.API
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool IsWindow(IntPtr hWnd);
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         private static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
+
+        [DllImport("user32.dll")]
+        static extern bool EnumThreadWindows(int dwThreadId, EnumThreadDelegate lpfn, IntPtr lParam);
         #endregion
 
     }
