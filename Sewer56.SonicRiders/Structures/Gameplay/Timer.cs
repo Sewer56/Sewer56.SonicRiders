b467536706b957720df807022e9278e6b317f782
Sewer56 <seweratwork@gmail.com>
2021-02-19 11:19:52 +0000

    Timer: Add Padding byte and Implicit Conversion

--- Sewer56.SonicRiders/Structures/Gameplay/Timer.cs
+++ Sewer56.SonicRiders/Structures/Gameplay/Timer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace Sewer56.SonicRiders.Structures.Gameplay
 {
@@ -7,15 +8,20 @@ namespace Sewer56.SonicRiders.Structures.Gameplay
         public byte Milliseconds;
         public byte Seconds;
         public byte Minutes;
+        public byte Unused;
 
         public Timer(byte milliseconds, byte seconds, byte minutes)
         {
             Milliseconds = milliseconds;
             Seconds = seconds;
             Minutes = minutes;
+            Unused = 0;
         }
 
         public TimeSpan ToTimeSpan() => new TimeSpan(0, 0, Minutes, Seconds, Milliseconds);
         public static Timer FromTimeSpan(TimeSpan span) => new Timer((byte)(span.Milliseconds / 10), (byte)span.Seconds, (byte)span.Minutes);
+
+        public static implicit operator Timer(int integer) => Unsafe.As<int, Timer>(ref integer);
+        public static implicit operator int(Timer timer) => Unsafe.As<Timer, int>(ref timer);
     }
 }
