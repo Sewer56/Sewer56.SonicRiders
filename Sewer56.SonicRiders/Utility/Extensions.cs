36bd9dc66e008f56cd6ec09f76952b27f9896752
Sewer56 <seweratwork@gmail.com>
2021-05-21 02:53:35 +0100

    Added: Utility Extension Function to Make Array from RefFixedArrayPtr<T>

---
+++ Sewer56.SonicRiders/Utility/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Reloaded.Memory.Pointers;
+
+namespace Sewer56.SonicRiders.Utility
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Copies items from a RefFixedArrayPtr to a managed array.
+        /// </summary>
+        public static T[] ToArray<T>(this RefFixedArrayPtr<T> items) where T : unmanaged
+        {
+            var result = new T[items.Count];
+            items.CopyTo(result, result.Length);
+            return result;
+        }
+
+    }
+}
