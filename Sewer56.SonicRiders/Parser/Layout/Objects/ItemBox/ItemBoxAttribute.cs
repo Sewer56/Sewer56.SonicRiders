d72a77c71457c1246bc6d2c399eef1737c2d345c
Sewer56 <seweratwork@gmail.com>
2021-06-07 19:11:08 +0100

    Added: List of Item Box Attributes

---
+++ Sewer56.SonicRiders/Parser/Layout/Objects/ItemBox/ItemBoxAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace Sewer56.SonicRiders.Parser.Layout.Objects.ItemBox
+{
+    public enum ItemBoxAttribute : short
+    {
+        Ring10,
+        Ring20,
+        Ring30,
+        Air30,
+        Air50,
+        Air100,
+        SpeedShoes,
+        Magnet,
+        Invincibility,
+        Bomb,
+        BallAndChain,
+        Random,
+        Ring100,
+        AirMax,
+    }
+}
