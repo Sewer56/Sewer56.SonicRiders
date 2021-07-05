a29a82724b6c14545bfb6955ac2d4d4088958339
Sewer56 <seweratwork@gmail.com>
2021-07-05 20:50:55 +0100

    Added: Map Portal (Object Visibility Bounding Box) Struct

---
+++ Sewer56.SonicRiders/Parser/Portal/Structs/Portal.cs
@@ -0,0 +1,41 @@
+﻿using Sewer56.SonicRiders.Parser.Layout.Structs;
+using Sewer56.SonicRiders.Utility.Math;
+
+namespace Sewer56.SonicRiders.Parser.Portal.Structs
+{
+    /// <summary>
+    /// Defines an individual "Object Portal"; which is a bounding box used to determine whether an object should be loaded.
+    /// If an object is in no Portal with the same ASCII character as assigned to the object (see <see cref="PortalChar"/> and <see cref="SetObject.PortalChar"/>)
+    /// </summary>
+    public struct Portal
+    {
+        /// <summary>
+        /// Minimum X,Y,Z
+        /// </summary>
+        public Vector3Short Minimum;
+
+        /// <summary>
+        /// Maximum X,Y,Z
+        /// </summary>
+        public Vector3Short Maximum;
+
+        /// <summary>
+        /// An ASCII character uniquely identifying this group of portals.
+        /// If a layout object is in no portal with the same character, it is not rendered.
+        /// </summary>
+        public byte PortalChar;
+        
+        /// <summary>
+        /// True if the object is rotated, else false.
+        /// </summary>
+        public byte IsRotated;
+
+        private short _padding;
+        
+        /// <summary>
+        /// Range 0 - 65535
+        /// i.e. 13684 = 90 degrees
+        /// </summary>
+        public Vector3Int RotationBams;
+    };
+}
---
+++ Sewer56.SonicRiders/Utility/Math/Vector3Short.cs
@@ -0,0 +1,24 @@
+﻿namespace Sewer56.SonicRiders.Utility.Math
+{
+    /// <summary>
+    /// Represents an integer vector composed of 3 elements.
+    /// </summary>
+    public struct Vector3Short
+    {
+        public short X;
+        public short Y;
+        public short Z;
+
+        public Vector3Short(short x, short y, short z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector3Short Add(Vector3Short other) => new Vector3Short((short) (X + other.X), (short) (Y + other.Y), (short) (Z + other.Z));
+        public Vector3Short Subtract(Vector3Short other) => new Vector3Short((short) (X - other.X), (short) (Y - other.Y), (short) (Z - other.Z));
+        public Vector3Short Multiply(Vector3Short other) => new Vector3Short((short) (X * other.X), (short) (Y * other.Y), (short) (Z * other.Z));
+        public Vector3Short Divide(Vector3Short other) => new Vector3Short((short) (X / other.X), (short) (Y / other.Y), (short) (Z / other.Z));
+    }
+}
