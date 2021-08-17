2564fe2ca93029743e26af9626e4052a47b25dca
Sewer 56 <seweratwork@gmail.com>
2021-08-17 21:30:39 +0100

    Added: Support for Archive Decompression

---
+++ Sewer56.SonicRiders/Parser/Archive/Helpers/BufferedStreamReaderByteStream.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using Reloaded.Memory.Streams;
+using Reloaded.Memory.Streams.Readers;
+using Sewer56.BitStream.Interfaces;
+
+namespace Sewer56.SonicRiders.Parser.Archive.Helpers
+{
+    public struct BufferedStreamReaderByteStream : IByteStream
+    {
+        public BufferedStreamReader Reader { get; private set; }
+        public BufferedStreamReaderByteStream(BufferedStreamReader reader) => Reader = reader;
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveOptimization | MethodImplOptions.AggressiveInlining)]
+        public byte Read(int index)
+        {
+            if (Reader.Position() != index)
+                Reader.Seek(index, SeekOrigin.Begin);
+
+            return Reader.Peek<byte>();
+        }
+
+        /// <inheritdoc />
+        public void Write(byte value, int index) => throw new System.NotImplementedException();
+    }
+}
