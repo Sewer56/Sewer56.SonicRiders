6a3bfc47e1a8e25884ddad315480a447e5d28672
Sewer 56 <seweratwork@gmail.com>
2021-09-14 05:52:11 +0100

    Player State: Make Decmal for Easier Reading

--- Sewer56.SonicRiders/Structures/Enums/PlayerState.cs
+++ Sewer56.SonicRiders/Structures/Enums/PlayerState.cs
@@ -2,119 +2,119 @@
 {
     public enum PlayerState : byte
     {
-        None = 0x00,
+        None = 0,
 
         /// <summary>
         /// The running state that is applied at the start of a race.
         /// </summary>
-        Running = 0x01,
+        Running = 1,
 
         /// <summary>
         /// Resets the player as if they were to fall out of map, go the wrong way, etc.
         /// </summary>
-        Reset = 0x03,
+        Reset = 3,
 
         /// <summary>
         /// Brings up the Retire Screen as if the current mission were to be failed by the player.
         /// </summary>
-        Retire = 0x04,
+        Retire = 4,
 
         /// <summary>
         /// Normally driving forward on extreme gear/skates/bike.
         /// </summary>
-        NormalOnBoard = 0x05,
+        NormalOnBoard = 5,
 
         /// <summary>
         /// Triggers a character jump.
         /// </summary>
-        Jump = 0x06,
+        Jump = 6,
 
         /// <summary>
         /// Applied when the player falls off a cliff/ledge without jumping.
         /// </summary>
-        FreeFalling = 0x07,
+        FreeFalling = 7,
 
         /// <summary>
         /// Doing Tricks (Horizontal Ramp) e.g. First Jump Metal City
         /// </summary>
-        TrickJumpHorizontal = 0x08,
+        TrickJumpHorizontal = 8,
 
         /// <summary>
         /// Doing Tricks (Vertical Ramp) e.g. First Jump Ice Factory
         /// </summary>
-        TrickJumpVertical = 0x09,
+        TrickJumpVertical = 9,
 
-        TrickJumpUnknown1 = 0x0A,
-        TrickJumpUnknown2 = 0x0B,
+        TrickJumpUnknown1 = 10,
+        TrickJumpUnknown2 = 11,
 
         /// <summary>
         /// Doing Tricks (Flat Vertical Ramp) (e.g. Ice Factory 2nd Jump), first jump after
         /// Metal City's first turn.
         /// </summary>
-        TrickJumpFlatVertical = 0x0C,
+        TrickJumpFlatVertical = 12,
 
         /// <summary>
         /// Doing Tricks (Turbulence) e.g. Turbulence
         /// </summary>
-        TrickJumpTurbulence = 0x0D,
+        TrickJumpTurbulence = 13,
 
         /// <summary>
         /// Turbulence
         /// </summary>
-        Turbulence = 0x10,
+        Turbulence = 16,
 
         /// <summary>
         /// Inside an Auto/Rotate Stick Section (or arrows on PC version).
         /// (Setting manually = crash, needs rail set somewhere first)
         /// </summary>
-        RotateSection = 0x11,
+        RotateSection = 17,
 
         /// <summary>
         /// Grinding (Setting manually = crash, needs rail set somewhere first)
         /// </summary>
-        Grinding = 0x12,
+        Grinding = 18,
 
         /// <summary>
         /// Flying (Flight Formation).
         /// </summary>
-        Flying = 0x13,
+        Flying = 19,
 
         /// <summary>
         /// Attacking an enemy/rival.
         /// (Setting manually = crash, needs enemy set somewhere first)
         /// </summary>
-        Attacking = 0x15,
+        Attacking = 21,
 
         /// <summary>
         /// Getting attacked by an enemy/rival.
         /// (Setting manually = crash, needs enemy set somewhere first)
         /// </summary>
-        GettingAttacked = 0x16,
+        GettingAttacked = 22,
 
         /// <summary>
         /// Running state after the player crosses the start line.
         /// </summary>
-        RunningAfterStart = 0x19,
+        RunningAfterStart = 25,
 
         /// <summary>
         /// Triggers the electric shock encountered if the player passes the start
         /// line too early.
         /// </summary>
-        ElectricShock = 0x1A,
+        ElectricShock = 26,
 
         /// <summary>
         /// Purpose unknown, brings player to a halt.
         /// </summary>
-        InstantStop = 0x1B,
+        InstantStop = 27,
 
         /// <summary>
         /// ElectricShock but longer.
         /// </summary>
-        ElectricShockLong = 0x1C,
+        ElectricShockLong = 28,
 
         /// <summary>
         /// Some variant of ElectricShock which crashes the game.
         /// </summary>
-        ElectricShockCrash = 0x1D,
+        ElectricShockCrash = 29,
     }
 }
